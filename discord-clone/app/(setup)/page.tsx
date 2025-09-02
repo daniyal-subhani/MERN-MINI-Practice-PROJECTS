216d88482ba227270854c0b643729bac5d75234e
daniyal-subhani <daniyalsubhani527@gmail.com>
2025-09-03 04:29:12 +0500

    Working on discord clone

---
+++ discord-clone/app/(setup)/page.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    this is a discord clone
+    <ModeToggle />
+   </div>
+  );
+}
