1190e5ca33a434c0142c4d2a4db1e22fe149f5ff
daniyal-subhani <daniyalsubhani527@gmail.com>
2025-09-04 10:52:15 +0500

    Add uploadthing in my app for media upoad, add core.ts

---
+++ discord-clone/app/api/uploadthing/core.ts
@@ -0,0 +1,27 @@
+import { auth } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+const handleAuth = async () => {
+  const userId = await auth();
+  if (!userId) throw new UploadThingError("Unauthorized");
+  return userId;
+};
+
+export const ourFileRouter = {
+  serverImage: f({
+    image: {
+      maxFileCount: 1,
+      maxFileSize: "4MB",
+    },
+  })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  messageFile: f(["pdf", "image"])
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
