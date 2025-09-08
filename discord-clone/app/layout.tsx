b59c2312a80b817aef74daf31fa4aacb8a3c2a31
daniyal-subhani <daniyalsubhani527@gmail.com>
2025-09-08 16:42:55 +0500

    Add shadcn and next/dark themes

--- discord-clone/app/layout.tsx
+++ discord-clone/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${cn(
+          geistSans.variable,
+          geistMono.variable,
+          "bg-white dark:bg-gray-700/50"
+        )} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          enableSystem
+          attribute={"class"}
+          defaultTheme="dark"
+          storageKey="discord_practice_proj"
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
