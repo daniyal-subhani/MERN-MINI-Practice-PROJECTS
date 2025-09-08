df3f021a737bf66caed23459e8276c6cf7153898
daniyal-subhani <daniyalsubhani527@gmail.com>
2025-09-08 17:04:03 +0500

    Add clerk auth, and setup middleware, create pages

--- discord-clone/app/(setup)/page.tsx
+++ discord-clone/app/(setup)/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-   <div className="text-center">Hello This is homepage.</div>
+   <div className="text-center">Hello This is homepage.
+   </div>
+
   );
 }
--- discord-clone/app/layout.tsx
+++ discord-clone/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
+
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${cn(
@@ -43,5 +46,6 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
