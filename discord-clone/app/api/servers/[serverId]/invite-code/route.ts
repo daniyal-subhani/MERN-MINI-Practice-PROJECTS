1abf69f45a39a6e40739cb622fc1e1c2e888d814
daniyal-subhani <daniyalsubhani527@gmail.com>
2025-09-14 18:51:34 +0500

    Create functionality for invite and join the server - with unique inviteCode

--- discord-clone/app/api/servers/[serverId]/invite-code/route.ts
+++ discord-clone/app/api/servers/[serverId]/invite-code/route.ts
@@ -1,33 +1,32 @@
-import { db } from '@/lib/db-hot-reloading';
-import { getCurrentProfile } from '@/lib/getCurrent-profile';
-import { NextResponse } from 'next/server';
-import {v4 as uuidv4} from "uuid";
+import { db } from "@/lib/db-hot-reloading";
+import { getCurrentProfile } from "@/lib/getCurrent-profile";
+import { NextResponse } from "next/server";
+import { v4 as uuidv4 } from "uuid";
 
 export async function PATCH(
-req: Request,
-{params}: {params: {serverId: string}}
-)  {
-try {
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
     const profile = await getCurrentProfile();
-    if(!profile) {
-        return new NextResponse("Unauthorized", {status: 401})
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
-    if(!params.serverId) {
-        return new NextResponse("Server id isn't provided", {status: 401})
+    if (!params.serverId) {
+      return new NextResponse("Server id isn't provided", { status: 401 });
     }
-    const server =await db.server.update({
-        where: {
-            id: params.serverId,
-            profileId: profile.id,
-            
-        },
-        data: {
-            inviteCode: uuidv4()
-        }
-    })
-    return NextResponse.json(server)
-} catch (error) {
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        inviteCode: uuidv4(),
+      },
+    });
+    return NextResponse.json(server);
+  } catch (error) {
     console.log("[SERVER_ID_PATCH]", error);
-    return new NextResponse("Internal error", {status: 500})
-}
+    return new NextResponse("Internal error", { status: 500 });
+  }
 }
